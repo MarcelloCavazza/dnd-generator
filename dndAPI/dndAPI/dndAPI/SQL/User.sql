3895fce7b3e59712d1f253479dc5d19433dc4446
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-02-07 06:46:50 -0300

    arrumando o sql par atabela de usuario

---
+++ dndAPI/dndAPI/dndAPI/SQL/User.sql
@@ -0,0 +1,15 @@
+﻿use dnd;
+
+create table user(
+id varchar(200) not null primary key,
+nickName varchar(100) not null,
+email varchar(100) not null,
+password varchar (100) not null,
+createdAt timestamp not null default current_timestamp,
+updatedAt timestamp on update current_timestamp,
+deleted boolean
+)engine=INNODB;
+
+select * from user
+
+drop table user
