287447897032db8630eca64addc23be78d0e2f55
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-02-08 06:33:31 -0300

    setting up everthing https://www.learmoreseekmore.com/2022/11/dotnet7-api-crud-efcore.html

---
+++ dndAPI/Program.cs
@@ -0,0 +1,31 @@
+using dndAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MainDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MainDbContext");
+});
+
+// Add services to the container.
+//Data Source=(localdb)\MSSQL;Initial Catalog=DNDGENERATOR;Integrated Security=True;Connect Timeout=30
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
