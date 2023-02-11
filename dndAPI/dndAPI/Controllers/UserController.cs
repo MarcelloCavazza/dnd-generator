f0316ab21cc3c7225256ca2241d84e253d25929c
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-02-10 21:41:58 -0300

    Basic user controller

---
+++ dndAPI/dndAPI/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using dndAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dndAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<User>> GetUserById()
+        {
+            var actualUser = new User
+            {
+                Id = 1,
+                nickName = "John Doe",
+                email = "teste@gmail.com",
+                password = "12345",
+                deleted= false,
+            };
+            return Ok(actualUser);
+        }
+    }
+}
