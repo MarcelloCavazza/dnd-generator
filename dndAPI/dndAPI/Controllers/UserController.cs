833b2ee412e0d2a3a9a0306e6c0080fc8a872063
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-02-11 06:20:21 -0300

    making a search request

--- dndAPI/dndAPI/Controllers/UserController.cs
+++ dndAPI/dndAPI/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using dndAPI.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dndAPI.Controllers
@@ -8,18 +9,37 @@ namespace dndAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<User>> GetUserById()
-        {
-            var actualUser = new User
+        private static List<User> users = new List<User> {
+            new User
             {
                 Id = 1,
                 nickName = "John Doe",
-                email = "teste@gmail.com",
+                email = "John@gmail.com",
                 password = "12345",
                 deleted= false,
-            };
-            return Ok(actualUser);
+            },
+            new User
+            {
+                Id = 2,
+                nickName = "Sylas",
+                email = "sylas@gmail.com",
+                password = "31231",
+                deleted= false,
+            }
+    };
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetAllUsers()
+        {
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(int id)
+        {
+            var userSelect = users.Find(user => user.Id == id);
+            if(userSelect is null) return NotFound("Sorry, we did not found the user you are looking for.");
+
+            return Ok(userSelect);
         }
     }
 }
