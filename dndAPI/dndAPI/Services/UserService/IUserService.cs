b5f7539e6294dc1093fc8a7b90e4d72bc55638fb
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-02-12 07:22:41 -0300

    made user service interface

---
+++ dndAPI/dndAPI/Services/UserService/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using dndAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dndAPI.Services.UserService
+{
+    public interface IUserService
+    {
+        List<User> GetAllUsers();
+        User GetUserById(int id);
+        List<User> AddUser([FromBody] User user);
+        User UpdateUser([FromBody] User user, int id);
+        User DeleteUser(int id);
+    }
+}
